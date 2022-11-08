cd700284d0216f75bd4078ad12f1581f412249b1
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-08 23:13:34 +0200

    Initial commit

---
+++ лаба 1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Горлачев средний уровень 
+double q = 2;
+double b = 1.8;
+double e = 2.71828182846;
+double t = 0;
+t = Math.Pow(b, 3) + Math.Pow(Math.Exp(e), Math.Sqrt(q));
+double x = Math.Pow(t, 3) + Math.Pow(b, 2);
+double y = Math.Pow(Math.Atan(Math.Abs(x)), 2);
+Console.WriteLine("Ответ y:"+y);
+Console.WriteLine("Ответ x:"+x);
+Console.WriteLine("Ответ t:"+t);
